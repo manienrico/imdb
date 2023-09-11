e645829874cbce939364ab8b6eb5723cb11cddf8
Enrico Mani <manienrico13@gmail.com>
2023-09-11 11:46:59 +0300

    add dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Providers from "./Providers"
 import Header from './components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
@@ -13,13 +14,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      {/* Header */}
-      <Header />
+        <Providers>
+          {/* Header */}
+          <Header />
 
-      {/* Navbar */}
+            {/* Navbar */}
 
-      {/* SearchBox */}
-      {children}
+            {/* SearchBox */}
+            {children}
+        </Providers>
+     
       </body>
     </html>
   )
