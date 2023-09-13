dbbeff2fd6e4e0d2d338c4ce2e92dd73bbc3b5af
Enrico Mani <manienrico13@gmail.com>
2023-09-13 11:16:48 +0300

    create about page

---
+++ src/app/about/page.js
@@ -0,0 +1,25 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>His having within saw become ask passed misery giving. 
+      Recommend questions get too fulfilled. He fact in we case 
+      miss sake.</p>
+      <p> Entrance be throwing he do blessing up. Hearts 
+      warmth in genius do garden advice mr it garret. Collected 
+      preserved are middleton dependent residence but him how. 
+      Handsome weddings yet mrs you has carriage packages. 
+      Preferred joy agreement put continual elsewhere delivered 
+      now. Mrs exercise felicity had men speaking met. 
+      Rich deal mrs part led pure will but.</p>
+
+      <p>Comfort reached gay perhaps chamber his six detract 
+      besides add. Moonlight newspaper up he it enjoyment 
+      agreeable depending. Timed voice share led his widen noisy 
+      young. On weddings believed laughing although material do 
+      exercise of. Up attempt offered ye civilly so sitting to. 
+      She new course get living within elinor joy. She her 
+      rapturous suffering concealed.</p>
+    </div>
+  )
+}
