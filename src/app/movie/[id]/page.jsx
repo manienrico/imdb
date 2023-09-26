0a56eeb9547f78bfae84d679d81891906f00678f
Enrico Mani <manienrico13@gmail.com>
2023-09-26 16:04:49 +0300

    add movie page, movie page structure

---
+++ src/app/movie/[id]/page.jsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+async function getMovie(movieId){
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`)
+    return await res.json();
+}
+
+export default async function MoviePage({ params }) {
+    const movieId = params.id;
+    const movie = await getMovie(movieId)
+  return (
+    <div className="w-full ">
+        <div className="p-4 md:pt-8 flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6">
+            <Image
+                src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`}
+                width={500}
+                height={300}
+                className="rounded-lg"
+                style={
+                {
+                    maxWidth: "100%",
+                    height: "100%",
+                }
+                }
+                placeholder="blur"
+                blurDataURL="/spinner.svg"
+                alt="Movie poster"
+            />
+        </div>
+    </div>
+  )
+}
